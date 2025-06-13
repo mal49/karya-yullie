d3beb83bbceb7e39e664dd4d5967625603faa116
mal49 <ikhmalhanif60@gmail.com>
2025-06-13 20:00:12 +0800

    addressed dark mode issued

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainPage from "./pages/mainPage";
 
 export default function App() {
   return(
-    <div className="min-h-screen bg-[#FCFFE3] font-roboto m-0 p-0">
+    <div className="min-h-screen bg-[#FCFFE3] font-roboto m-0 p-0 dark:bg-[#FCFFE3]">
       <MainPage/>
     </div>
   );
